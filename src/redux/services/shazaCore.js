451d5aca8fc2d6a6ba99f94c509fc0134e086ea3
Eduardo Duarte <eduardoievolucao@gmail.com>
2022-09-26 06:02:27 -0300

    first commit

---
+++ src/redux/services/shazaCore.js
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'b273fea926mshac4b29596ca3df2p1d0439jsn3252d7b13eb5',
+      'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
+    }
+  };
+  
+  fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+    export const shazamCoreApi = createApi({
+        reducerPath: 'shazamCoreApi',
+    })
